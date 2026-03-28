f7902b67bf0bed0ee21dcf558b9d49b21209021b
danila.p <danilaprokoshev@gmail.com>
2026-03-28 13:14:51 +0300

    refactor: rename baseApi reducer path to api and remove unused useUsers hook

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
-  reducerPath: 'baseApi',
+  reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://jsonplaceholder.typicode.com',
   }),
